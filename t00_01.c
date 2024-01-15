fa1690984a193dd38744bd53fe0a76a19ee886ea
gracecalderasitumorang <gracecalderas@gmail.com>
2024-01-15 11:16:53 +0700

    12S23048

--- t00_01.c
+++ t00_01.c
@@ -1,9 +1,10 @@
-// NIM - NAMA
+// 12S23048 - Grace Caldera Situmorang
 
 #include <stdio.h>
 
 int main(char **_argc, int _argv)
 {
-    // Show your code here ...
+    printf("Hello, world!");
+
     return 0;
 }
